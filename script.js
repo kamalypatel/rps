684a988572d242ad7cc5eab25a94ba890dfb002d
Kamal Patel <80398694+kamalypatel@users.noreply.github.com>
2024-03-03 18:54:45 -0500

    added a prompt for player selection and started function for the game

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 function getComputerChoice(){
    let randomNumber = Math.random()
    if (randomNumber < 0.33) {
-    return 'Rock'
+    return 'rock'
    } else if (randomNumber < 0.66) {
-    return 'Paper'
+    return 'paper'
    } else {
-    return 'Scissors'
+    return 'scissors'
    }
 }
 
 console.log(getComputerChoice())
+
+let playerSelection = prompt('What is your game choice? (Rock, Paper, Scissors').toLowerCase()
+
+console.log(playerSelection)
+
+let computerSelection = getComputerChoice()
+
+function playGame(computerSelection, playerSelection){
+    if (computerSelection == playerSelection) {
+        return 'Game is a tie!'
+    } else if ((computerSelection == 'rock' && playerSelection == 'scissors')
+    || (playerSelection == 'scissors' && computerSelection == 'paper')
+    || (playerSelection == 'paper' && computerSelection == 'rock')) {
+        return string('You win! You picked' + playerSelection + ' and the compter picked ' + computerSelection)
+    }
+}
